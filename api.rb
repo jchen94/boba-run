70356625532ae44fec08613dfa7d39065360ee4c
jchen94 <jchen94@ucla.edu>
2016-05-30 02:46:24 -0700

    another sign up bug

--- api.rb
+++ api.rb
@@ -41,7 +41,7 @@ class API < Sinatra::Base
     else
     	@user = User.new
       @user.first_name = params['first_name']
-      @user.first_name = params['last_name']
+      @user.last_name = params['last_name']
     	@user.username = params['username']
     	@user.password = params['password']
       @user.email = params['email']
